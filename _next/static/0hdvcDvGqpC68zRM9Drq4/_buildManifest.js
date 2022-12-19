5343c238624d0443bcda4fe6f361dc87e7e05b33
theinitium-alien <alien@theinitium.com>
2022-12-19 15:58:36 +0800

    feat: improve performance

---
+++ _next/static/0hdvcDvGqpC68zRM9Drq4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/899-8b27870a0eb1872a.js","static/chunks/pages/index-4a9e80209785cc96.js"],"/_error":["static/chunks/pages/_error-4b61be14865575c5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
