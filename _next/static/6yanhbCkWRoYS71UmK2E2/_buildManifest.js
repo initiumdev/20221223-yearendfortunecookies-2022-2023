02f0d84e4fe729018ab612e156732f9d2a3018a1
theinitium-alien <alien@theinitium.com>
2022-12-23 18:31:58 +0800

    fix: absolute path

---
+++ _next/static/6yanhbCkWRoYS71UmK2E2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/737-4d6d653dc3af0400.js","static/chunks/pages/index-e5d538abfe5abc3f.js"],"/_error":["static/chunks/pages/_error-4b61be14865575c5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
